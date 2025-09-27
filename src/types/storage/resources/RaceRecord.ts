dc7d1e2730f41880d1536455c2114e10315ee45f
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-09-27 19:01:01 +0200

    feat: add storage layer for current supported homebrew object types

---
+++ src/types/storage/resources/RaceRecord.ts
@@ -0,0 +1,60 @@
+import { AbilityBonusRecord } from '../helpers/AbilityBonusRecord.js';
+import { ChoiceRecord } from '../helpers/ChoiceRecord.js';
+import { BaseResourceRecord } from '../wrappers/BaseResourceRecord.js';
+
+export interface Race extends BaseResourceRecord {
+	/**
+	 * Speed of the race in feet per round.
+	 */
+	speed: number;
+
+	/**
+	 * Bonuses to abilities this race gives.
+	 */
+	ability_bonuses: AbilityBonusRecord[];
+
+	/**
+	 * Flavor description of the typical age of the race.
+	 */
+	age: string;
+
+	/**
+	 * Flavor description of the typical alignments of the race.
+	 */
+	alignment: string;
+
+	/**
+	 * In-game size of the race.
+	 */
+	size: string;
+
+	/**
+	 * Flavor description of the typical size of the race.
+	 */
+	size_description: string;
+
+	/**
+	 * Starting languages for all new characters of this race.
+	 */
+	languages: BaseResourceRecord[];
+
+	/**
+	 * Starting language options for all new characters of this race.
+	 */
+	language_options?: ChoiceRecord;
+
+	/**
+	 * Flavor description of the languages known by the race.
+	 */
+	language_desc: string;
+
+	/**
+	 * A list of traits the race has.
+	 */
+	traits: BaseResourceRecord[];
+
+	/**
+	 * A list of subraces the race has, if any.
+	 */
+	subraces: BaseResourceRecord[];
+}
---
+++ src/types/storage/wrappers/BaseResourceRecord.ts
@@ -0,0 +1,23 @@
+/**
+ * Base interface for all D&D 5e homebrew resource records.
+ * Provides common properties shared by all game resources like classes, races, spells, etc.
+ * Only used for storage.
+ */
+export interface BaseResourceRecord {
+	/**
+	 * Unique identifier for the resource.
+	 * UUID.
+	 */
+	id: string;
+
+	/**
+	 * Human-readable display name of the resource.
+	 */
+	name: string;
+
+	/**
+	 * Type of the resource indicating its category.
+	 * @example "classes", "races", "monsters"
+	 */
+	resourceType: string;
+}
