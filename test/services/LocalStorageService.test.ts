21ea4479330a829ecf99ba5d0ed9d66ece5afa47
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-10-09 19:20:21 +0200

    test: add test outline for LocalStorageService.test.ts

---
+++ test/services/LocalStorageService.test.ts
@@ -0,0 +1,45 @@
+import { describe, it } from 'vitest';
+
+describe('get', () => {
+	it.skip('should return a value for existing key', () => {
+		// TODO
+	});
+
+	it.skip('should return undefined for non-existing key', () => {
+		// TODO
+	});
+
+	it.skip('should map to correct type', () => {
+		// TODO
+	});
+});
+
+describe('set', () => {
+	it.skip('should store a value for a key', () => {
+		// TODO
+	});
+
+	it.skip('should overwrite an existing value for a key', () => {
+		// TODO
+	});
+});
+
+describe('delete', () => {
+	it.skip('should remove a value for a key', () => {
+		// TODO
+	});
+
+	it.skip('should do nothing for a non-existing key', () => {
+		// TODO
+	});
+});
+
+describe('getAllKeys', () => {
+	it.skip('should return an array of all keys', () => {
+		// TODO
+	});
+
+	it.skip('should return an empty array if no keys exist', () => {
+		// TODO
+	});
+});
