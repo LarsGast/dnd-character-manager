3e42c4baa1acf898879a3149a6623f75f8fa29d7
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-10-09 18:58:50 +0200

    chore: remove unused test file

--- test/hello.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-
-// A simple test to verify that the testing setup is working.
-// Needed because there are currently no other tests.
-describe('Dummy Tests', () => {
-	it('should pass', () => {
-		expect(true).toBe(true);
-	});
-});
